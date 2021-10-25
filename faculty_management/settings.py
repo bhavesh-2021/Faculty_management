47fff78f79ee797e5bfe15b1fb877887fe254e99
bhavesh-2021 <bhaveshjagdishsharma1300@gmail.com>
2021-10-25 10:48:21 +0530

    Update settings.py

--- faculty_management/settings.py
+++ faculty_management/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'facultymanagement2020@gmail.com'
-EMAIL_HOST_PASSWORD = 'medicaps'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
